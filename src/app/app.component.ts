74f6b83c625b0a906121216c7984b13147597bf9
victor <v.alcantara@celonis.com>
2021-01-21 20:09:17 +0100

    creating photos component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pictures';
+  title = "Pictures Web Application";
+  photos = [
+    {
+      url: 'https://images.ctfassets.net/hrltx12pl8hq/4f6DfV5DbqaQUSw0uo0mWi/ff068ff5fc855601751499d694c0111a/shutterstock_376532611.jpg?fit=fill&w=800&h=300',
+      description: 'Leão'
+    },
+    {
+      url: 'https://image.freepik.com/free-photo/sunset-lake-beautiful-sunset-clouds-lake-landscape-background_9635-881.jpg',
+      description: 'Leoa'
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PhotosModule } from './photos/photos.module';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
