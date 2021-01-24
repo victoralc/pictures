6a145ec46bb99dd0e32b1ba77820d26a250ab375
victoralc <victoralcantara432@gmail.com>
2021-01-24 14:34:37 +0100

    token and headers implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 import { ErrorsModule } from './errors/errors.module';
 
 import { PhotosModule } from './photos/photos.module';
@@ -16,7 +17,8 @@ import { PhotosModule } from './photos/photos.module';
     BrowserModule,
     AppRoutingModule,
     PhotosModule,
-    ErrorsModule
+    ErrorsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { TokenService } from './token/token.service';
 
 const API_URL = 'http://localhost:3000/user/login'
 
@@ -9,10 +11,18 @@ const API_URL = 'http://localhost:3000/user/login'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient, 
+    private tokenService: TokenService) { }
 
   authenticate(userName: string, password: string){
-    return this.http.post(API_URL, {userName, password});    
+    return this.http
+      .post(API_URL, {userName, password}, {observe: "response"})
+      .pipe(tap(res => {1
+        const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken);
+        console.log(`Use ${userName} authenticated with token ${authToken}`);
+      }));    
   }
 
 }
