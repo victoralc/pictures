ebef2b518a5a2b6d56b769f22c17ce24297e02fa
victoralc <victoralcantara432@gmail.com>
2021-01-25 23:59:53 +0100

    BehaviorSubject, Async pipe and logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/auth/auth.guard';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { SignInComponent } from './home/signin/signin.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
@@ -8,7 +9,7 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 
 const routes: Routes = [
-  { path: '', component: SignInComponent },
+  { path: '', component: SignInComponent, canActivate: [AuthGuard] },
   { path: 'user/:userName', component: PhotoListComponent , 
     resolve: {
       photos: PhotoListResolver
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
+import { UserService } from '../user/user.service';
 
 @Component({
     selector: 'app-header',
@@ -6,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
     
+    user$: Observable<User>;
+
+    constructor(
+        private userService: UserService, 
+        private router: Router ) {
+
+        this.user$ = userService.getUser();
+    }
+
+    logout(){
+        this.userService.logout();
+        this.router.navigate(['']);
+    }
+
 }
